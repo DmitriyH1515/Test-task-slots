224b8956eee7c725512e2f692658f431161bed57
Dima Havrylenko <havrylenko.dima@icloud.com>
2022-06-14 19:26:24 +0300

    Initial Commit

---
+++ Test task slots/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test task slots
+//
+//  Created by Dmitriy Havrylenko on 14.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
